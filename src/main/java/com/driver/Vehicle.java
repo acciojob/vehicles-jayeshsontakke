0e6f2776a0ddb0a3e3f6f47a68f40ede8a258632
jayeshsontakke <jayeshsontakke1998@gmail.com>
2023-03-18 12:16:45 +0530

    case study

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Vehicle {
 
-    private String name;
-    private int currentSpeed;
-    private int currentDirection;
+    public String name;
+    public int currentSpeed;
+    public int currentDirection;
 
     public Vehicle(String name) {
 
